24a5a0ffcc88e58ca13acdfd5a8dc5ed900cf227
Eddi <wollas@inbox.ru>
2026-02-01 12:43:56 +0500

    feat(posts):Добавил функционал списка постов: - шаблон со списком постов - маршрут и обработчик для него

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Post(models.Model):
-  title = models.CharField(max_length=200)
-  text = models.TextField()
+  title = models.CharField(max_length=200, verbose_name='Заголовок')
+  text = models.TextField(verbose_name='Текст')
   created_at = models.DateTimeField(auto_now_add=True)
 
   class Meta:
@@ -10,5 +10,5 @@ class Post(models.Model):
     verbose_name_plural = 'Посты'
     db_table = 'blog_posts'
 
-    def __str__(self):
-      return self.title
+  def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from blog.models import Post
 
-def home_view(request):
-  return HttpResponse('Главная страница')
+# def home_view(request):
+#   return HttpResponse('Главная страница')
+
+def get_post_list(request):
+  posts = Post.objects.all()
+
+  return render(request, template_name='blog/post_list.html', context={'posts': posts}) 
