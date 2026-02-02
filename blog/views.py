42057d79059277b4f2d84b4756b8f8e940fd6af4
Eddi <wollas@inbox.ru>
2026-02-02 18:30:33 +0500

    feat(posts):Добавил функционал получения добавления поста(самый простой вариант)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 # from django.http import HttpResponse
 from blog.models import Post
 
@@ -16,3 +16,13 @@ def get_post_detail(request, post_id):
 
   # return render(request, 'blog/post_detail.html', {'post': Post.objects.get(id=post_id)})
   return render(request, 'blog/post_detail.html', {'post': get_object_or_404(Post, id=post_id)})
+
+
+def create_post(request):
+  if request.method == "GET":
+    return render(request, 'blog/post_add.html')
+  
+  if request.method == "POST":
+    post = Post.objects.create(title=request.POST.get('title'), text=request.POST.get('text'))
+
+    return redirect('post_detail', post_id=post.id)
