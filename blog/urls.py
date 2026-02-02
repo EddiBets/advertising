26080e6cef66dfee9786061427a7ec6bebd17040
Eddi <wollas@inbox.ru>
2026-02-02 18:37:56 +0500

    feat(nav):Добавил навигационное меню

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from .import views
 urlpatterns = [
     path('posts/', views.get_post_list, name='post_list'),
     path('posts/<int:post_id>', views.get_post_detail, name='post_detail'),
-    path('posts/add/', views.create_post), # name="new_post"
+    path('posts/add/', views.create_post, name="new_post"), # name="new_post"
 ]
