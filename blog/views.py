a584f63e93b770aefbfc29f7dddb94702b7e76c9
Eddi <wollas@inbox.ru>
2026-02-07 10:23:11 +0500

    feat(posts):Оптимизировал вьюшку создания поста (forms.Form)

--- blog/views.py
+++ blog/views.py
@@ -18,9 +18,9 @@ def get_post_detail(request, post_id):
 
 
 def create_post(request):  
-  if request.method == "POST":
-    form = PostForm(request.POST)
+  form = PostForm(request.POST or None)
 
+  if request.method == "POST":
     if form.is_valid():
       post = Post.objects.create(
         title=form.cleaned_data['title'],
@@ -28,11 +28,6 @@ def create_post(request):
       )
       
       return redirect('post_detail', post_id=post.id)
-    else:
-      return render(request, 'blog/post_add.html', {"form": form})
 
-    
-  form = PostForm()
-  
   
   return render(request, 'blog/post_add.html', {"form": form})
