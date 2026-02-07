7126fc8c779ed094711f69eaed69b7b8a76cfc3e
Eddi <wollas@inbox.ru>
2026-02-07 08:38:10 +0500

    feat(posts):Добавил обработку ошибок в форму создания поста - Отображение ошибок в шаблоне - Значения полей не сбрасываются при повторном рендере

--- blog/views.py
+++ blog/views.py
@@ -23,6 +23,28 @@ def create_post(request):
     return render(request, 'blog/post_add.html')
   
   if request.method == "POST":
-    post = Post.objects.create(title=request.POST.get('title'), text=request.POST.get('text'))
+    title = request.POST.get('title').strip()
+    text = request.POST.get('text').strip()
 
-    return redirect('post_detail', post_id=post.id)
+    errors ={}
+
+    if not title:
+      errors['title'] = 'Вы забыли заголовок?'
+    if not text:
+      errors['text'] = 'Вы забыли слова?'
+    
+    if not errors:
+      post = Post.objects.create(title=title, text=text)
+
+
+    # post = Post.objects.create(title=request.POST.get('title'), text=request.POST.get('text'))
+
+      return redirect('post_detail', post_id=post.id)
+    else:
+      context = {
+        'errors': errors,
+        'title': title,
+        'text': text
+    }
+
+    return render(request, 'blog/post_add.html', context)
