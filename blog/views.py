3cc76b64edbb7c34396a9bd875fd221bee10b8ed
Eddi <wollas@inbox.ru>
2026-02-09 05:56:41 +0500

    feat(urls): Добавил параметр app_name в маршруты приложенй

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,7 @@ def create_post(request):
       post = form.save()
       
 
-      return redirect('post_detail', post_id=post.id)
+      return redirect('blog:post_detail', post_id=post.id)
 
   
   return render(request, 'blog/post_form.html', {"form": form, 'title': title, 'submit_button_text': submit_button_text})
@@ -47,7 +47,7 @@ def update_post(request, post_id):
     if form.is_valid():
       updated_post = form.save()
 
-      return redirect('post_detail', post_id=post.id)
+      return redirect('blog:post_detail', post_id=post.id)
     else:
       return render(request, 'blog/post_form.html', context={"form": form, 'title': title, 'submit_button_text': submit_button_text})
     
@@ -67,6 +67,6 @@ def delete_post(request, post_id):
   if request.method == "POST":
     post.delete()
 
-    return redirect('post_list')
+    return redirect('blog:post_list')
 
   return render(request, 'blog/confirm_post_delete.html', {'post':post})
