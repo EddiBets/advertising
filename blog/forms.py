0fabf00ce60ca78c06350e1434072cd67da6044d
Eddi <wollas@inbox.ru>
2026-02-13 09:56:10 +0500

    feat(posts/tags): Ручная обработка тегов: изменил связанный с моделью tags в PostForm на несвязанный с моделью tags_input

--- blog/forms.py
+++ blog/forms.py
@@ -3,21 +3,28 @@ from django import forms
 from blog.models import Post
 
 class PostForm(forms.ModelForm):
+  tags_input = forms.CharField(
+    required=False,
+    widget=forms.TextInput(attrs={
+      'class': 'form-control',
+      'placeholder': 'Введите теги через запятую'
+    }),
+    label="Теги"
+  )
+
   class Meta:
     model = Post
-    fields = ['title', 'category','tags', 'text', 'image']
+    fields = ['title', 'category', 'text', 'image']
     widgets = {
       'title': forms.TextInput(attrs={
         'placeholder': "заголовок (максимальная длина 200 символов)"
       }),
       'category': forms.Select(attrs={'class': 'form-control'}),
-      'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
       'image': forms.FileInput(attrs={'class': 'form-control'})
     }
     labels = {
       'title': 'Заголовок поста',
       'category': 'Категория:',
-      'tags': 'Теги:',
       'text' : 'Текст поста',
       'image': 'Картинка поста'
     }
@@ -36,3 +43,13 @@ class PostForm(forms.ModelForm):
       raise forms.ValidationError("Заголовок не должен быть короче 5 символов")
     
     return title
+  
+  def clean_tags_input(self):
+    """
+    Разбивает строку на список тегов:
+    - удаляет лишние пробелы вокруг
+    - приводит к нижнему регистру
+    """
+    tags_str = self.cleaned_data.get('tags_input')
+    tags = [tag.strip().lower() for tag in tags_str.split(',') if tag.strip()]
+    return tags
