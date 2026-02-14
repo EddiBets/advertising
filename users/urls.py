3e5542ee8e9a84f551c3df38475a8dcfcb841ba4
Eddi <wollas@inbox.ru>
2026-02-14 21:44:03 +0500

    refactor(users/views.py): Переписал logout_view на CBV

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,8 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', views.CustomLoginView.as_view(), name="login"),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+
     path("<str:username>/", views.profile_view, name='profile'),
 ]
